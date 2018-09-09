b3ea5682345a1618da1befcb48fd99bf81235715
gothicrush <gothicrush@outlook.com>
2018-09-09 13:32:37 +0800

    添加 Visitor

--- assist/assist.go
+++ assist/assist.go
@@ -1,7 +1,13 @@
 package assist
 
+// 自定义比较器
 type Comparator func (i interface{}, j interface{}) int
 
+
+// 比较函数，第1,2个参数接收需要比较的值，第3个参数传入自定义的比较器
 func Compare(i interface{}, j interface{}, f func (i interface{}, j interface{}) int ) int {
 	return f(i,j)
 }
+
+// 自定义访问器
+type Visitor func (v interface{}) 
